890c8145c77d88eca03b21d47c9ec5bd6283c71b
Tito Alvarez <augustoalvarez@gmail.com>
2016-12-17 09:33:15 -0600

    Base RoboFile and configuration files for testing

---
+++ src/Tasks/CodeceptBuild.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package     Joomla\Testing\Robo
+ * @subpackage  Tasks
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Testing\Robo\Tasks;
+
+use Robo\Task\Testing\Codecept;
+
+/**
+ * Builder tasks for Codeception (based on Robo's Codecept task)
+ *
+ * @package     Joomla\Testing\Robo
+ * @subpackage  Tasks
+ *
+ * @since       1.0.0
+ */
+final class CodeceptBuild extends Codecept
+{
+	/**
+	 * Constructor function - based on Codecept constructor
+	 *
+	 * @param   string  $pathToCodeception  Path to codeception, optional
+	 *
+	 * @throws \Robo\Exception\TaskException
+	 *
+	 * @since   1.0.0
+	 */
+	public function __construct($pathToCodeception = '')
+	{
+		// No parent constructor call since the command will be generated manually
+
+		$this->command = $pathToCodeception;
+
+		if (!$this->command)
+		{
+			$this->command = $this->findExecutable('codecept');
+		}
+
+		if (!$this->command)
+		{
+			throw new TaskException(__CLASS__, "Neither composer nor phar installation of Codeception found.");
+		}
+
+		$this->command .= ' build';
+	}
+
+	/**
+	 * Generates AcceptanceTester
+	 *
+	 * @return  \Robo\Result
+	 *
+	 * @since   1.0.0
+	 */
+	public function run()
+	{
+		$command = $this->getCommand();
+		$this->printTaskInfo('Executing {command}', ['command' => $command]);
+
+		return $this->executeCommand($command);
+	}
+}
