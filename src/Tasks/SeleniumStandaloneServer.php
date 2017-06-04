2b2f755500cd1fd6eb4e8e6ed878d4d8562eff27
Tito Alvarez <augustoalvarez@gmail.com>
2017-06-04 07:04:56 +0200

    Removed debug information

--- src/Tasks/SeleniumStandaloneServer.php
+++ src/Tasks/SeleniumStandaloneServer.php
@@ -253,8 +253,6 @@ final class SeleniumStandaloneServer extends GenericTask
 			$command .= (($this->debug) ? ' -debug' : '');
 		}
 
-		$this->printTaskInfo('Selenium full command: ' . $command);
-
 		if (!$roboHandler->executeDaemon($command))
 		{
 			$this->printTaskError('Selenium server execution failed');
