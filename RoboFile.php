a20198113a145e223827fd13c312826cf4a80620
Isac Andrei <isacandrei19@gmail.com>
2017-07-04 22:00:31 +0300

    fix codestyle

--- RoboFile.php
+++ RoboFile.php
@@ -27,7 +27,7 @@ define('JPATH_TESTING_BASE', __DIR__);
  */
 class RoboFile extends \Robo\Tasks
 {
-	use Joomla\Testing\Robo\Tasks\loadTasks;
+	use Joomla\Testing\Robo\Tasks\LoadTasks;
 
 	/**
 	 * Check the code style of the project against a passed sniffers using PHP_CodeSniffer_CLI
@@ -53,11 +53,6 @@ class RoboFile extends \Robo\Tasks
 			->setCodeStyleCheckFolders(
 				array(
 					'src'
-				)
-			)
-			->setCodeStyleExcludedPaths(
-				array(
-					'src/Tasks/loadTasks.php'
 				)
 			)
 			->checkCodeStyle()
